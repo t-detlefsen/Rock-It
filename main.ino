55c2fba6272b5a6e2826bc29412f847d4786117f
t-detlefsen <70849599+t-detlefsen@users.noreply.github.com>
2021-10-19 13:59:52 -0400

    Add files via upload

---
+++ main.ino
@@ -0,0 +1,27 @@
+/*
+ * Thomas Detlefsen
+ * 10-11-2021
+ * main.ino
+*/
+
+// Intializations
+void setup()
+{  
+  // Set inputs & outputs
+  pinMode(8, OUTPUT);
+  pinMode(9, INPUT);
+}
+
+// Main Loop
+void loop()
+{  
+    // Check input
+    if (digitalRead(9))
+    {
+      // Blink light
+      digitalWrite(8, HIGH);
+      delay(500);
+      digitalWrite(8, LOW);
+      delay(500);
+    }
+}
