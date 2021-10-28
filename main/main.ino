0415021dd24d5e79e40c733ca9feecd745257931
Thomas Detlefsen <thomasdetlefsen@Thomas-Detlefsens-MacBook-Pro.local>
2021-10-27 23:59:11 -0400

    Speaker + Readme

---
+++ main/main.ino
@@ -0,0 +1,24 @@
+/*
+ * Thomas Detlefsen
+ * 10-27-2021
+ * main.ino
+*/
+
+// For Audio
+#include <pcmRF.h>
+#include <pcmConfig.h>
+#include <TMRpcm.h>
+
+// For SD Card
+#include "SD.h"
+#include "SPI.h"
+
+// Intializations
+void setup() {
+  music.speakerPin = 5;
+}
+
+// Main Loop
+void loop() {
+  
+}
