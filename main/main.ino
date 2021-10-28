89f701c8d4985faf162ad3835a105a62e04ac6b5
Thomas Detlefsen <thomasdetlefsen@Thomas-Detlefsens-MacBook-Pro.local>
2021-10-28 16:06:33 -0400

    LCD testing code

--- main/main.ino
+++ main/main.ino
@@ -4,12 +4,25 @@
  * main.ino
 */
 
+// For LCD
+#include "Wire.h"
+#include "Adafruit_LiquidCrystal.h"
+
+Adafruit_LiquidCrystal lcd(0);
+
 // Intializations
 void setup() {
-  
+  // LCD Output Setup
+  lcd.begin(16, 2);
+  lcd.print("hello, world");
 }
 
 // Main Loop
 void loop() {
-  
+  lcd.setCursor(0, 1);
+  lcd.print(millis()/1000);
+  lcd.setBacklight(HIGH);
+  delay(500);
+  lcd.setBacklight(LOW);
+  delay(500);
 }
